bfaacb4aabadd7b662fa1a9005453922c73ca788
ideabook <ideabook@163.com>
2013-09-05 18:01:02 +0800

    second

---
+++ src/test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Administrator
+ * @time 2013-9-5
+ * @className test
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test git");
+	}
+
+}
